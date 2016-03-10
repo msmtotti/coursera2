101391af1763938583e6641493f83eb13119c966
Miguel Soltero <msoltero@gabinete.mx>
2016-03-10 14:40:31 -0600

    Semana 4 de Swift: programar para iOS

---
+++ Semana4.playground/Contents.swift
@@ -0,0 +1,66 @@
+//: Velocimetro de un auto usando enum y class
+
+import UIKit
+
+enum Velocidades : Int{
+    case Apagado = 0
+    case VelocidadBaja = 20
+    case VelocidadMedia = 50
+    case VelocidadAlta = 120
+    
+    init( velocidadInicial : Velocidades){
+        self = velocidadInicial
+    }
+}
+
+class Auto {
+    var velocidad = Velocidades.Apagado
+    
+    init(){
+        velocidad = .Apagado
+    }
+    
+    func cambioDeVelocidad() -> (actual : Int, velocidadEnCadena : String) {
+        var valor : Int;
+        var texto : String;
+        switch velocidad{
+            
+        case .Apagado:
+            valor = velocidad.rawValue
+            texto = "Apagado"
+            velocidad = .VelocidadBaja
+            
+        case .VelocidadBaja:
+            valor = velocidad.rawValue
+            texto = "Velocidad baja"
+            velocidad = .VelocidadMedia
+            
+        case .VelocidadMedia:
+            valor = velocidad.rawValue
+            texto = "Velocidad media"
+            velocidad = .VelocidadAlta
+            
+        case .VelocidadAlta:
+            valor = velocidad.rawValue
+            texto = "Velocidad alta"
+            velocidad = .VelocidadMedia
+            
+        }
+        return (valor,texto)
+    }
+}
+
+var velocimetro = Auto()
+
+for cambios in 1...20{
+    let regreso = velocimetro.cambioDeVelocidad()
+    print("\(cambios). \(regreso.actual), \(regreso.velocidadEnCadena)")
+    
+}
+
+
+
+
+
+
+
